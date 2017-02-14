700bfe42accca89f8558ba05713b15e33aa42d46
Pavel Sharanda <edvaef@gmail.com>
2017-02-14 19:23:41 +0300

    flux

---
+++ 7. FLUX/FLUX/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  Created by Pavel Sharanda on 17.11.16.
+//  Copyright © 2016 psharanda. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
